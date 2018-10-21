2064adfe274e59339a49bd5d0ac900cccb32d08c
eckmLJE <lucas.eckman@gmail.com>
2018-10-21 16:56:42 -0400

    concert tiles looking good

--- src/Concerts/ConcertTile.js
+++ src/Concerts/ConcertTile.js
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import "./ConcertTile.css";
 
-const ConcertTile = props => {
-  return (
-    <div className="concert-tile">
-      <div className="concert-tile-inner">Tile</div>
-    </div>
-  );
-};
+import moment from "moment";
+
+class ConcertTile extends Component {
+  selectImage = () => {
+    const image = this.props.concert.images.find(image => image.width > 1000);
+    return image.url;
+  };
+
+  render() {
+    return (
+      <div className="concert-tile">
+        <div className="concert-tile-inner">
+          <div
+            className="tile-image"
+            style={{ backgroundImage: `url(${this.selectImage()}` }}
+          />
+          <div className="tile-details">
+            <div className="tile-event-details">
+              <h1>{this.props.concert.name}</h1>
+              <p>
+                {moment(this.props.concert.dates.start.dateTime).format(
+                  "MMMM Do YYYY, h:mm a"
+                )}
+              </p>
+              <p>{this.props.concert._embedded.venues[0].name}</p>
+            </div>
+            <div className="tile-plan-details" />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default ConcertTile;
