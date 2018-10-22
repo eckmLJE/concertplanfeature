7d7b8853e74b650debdcdefb4364226038d8f98b
eckmLJE <lucas.eckman@gmail.com>
2018-10-22 16:23:49 -0400

    added comments drawer

--- src/Plans/PlanViewPanel.js
+++ src/Plans/PlanViewPanel.js
@@ -1,21 +1,20 @@
-import React, { Fragment } from "react";
+import React from "react";
 import "./PlanViewPanel.css";
 
 const PlanViewPanel = props => (
-  <Fragment>
-    <div className="plan-view-panel">
-      <div className="plan-view-panel-inner">
-        <div className="view-panel-left" />
-        <div className="view-panel-right">
-          <div className="panel-right-top" />
-          <div className="panel-right-bottom">
-            <div className="right-bottom-left" />
-            <div className="right-bottom-right" />
-          </div>
+  <div className="plan-view-panel">
+    <div className="plan-view-panel-inner">
+      <div className="view-panel-left" />
+      <div className="view-panel-right">
+        <div className="panel-right-top" />
+        <div className="panel-right-bottom">
+          <div className="right-bottom-left" />
+          <div className="right-bottom-right" />
         </div>
       </div>
     </div>
-  </Fragment>
+    <div className="plan-view-comments-drawer" />
+  </div>
 );
 
 export default PlanViewPanel;
